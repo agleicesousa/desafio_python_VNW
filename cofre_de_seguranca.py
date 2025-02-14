4b6c69fb33c457214856f85ac69b721ebe139055
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-02-14 03:22:00 -0300

    feat: cofre de segurança

---
+++ cofre_de_seguranca.py
@@ -0,0 +1,8 @@
+def senha_do_cofre():
+    senha = input("Digite a senha: ")
+    if senha == "Python123":
+        print("Acesso permitido!")
+    else:
+        print("Acesso negado!")
+
+senha_do_cofre()
